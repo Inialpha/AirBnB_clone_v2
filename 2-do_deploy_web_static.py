3bd99b01f4982ce8de1bcdf2e8264da14ae8b9b1
Initech <inimfonebong001@gmail.com>
2023-10-04 13:47:14 -0700

    2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive
+to web servers, using the function do_deploy"""
+from fabric.api import run, put, env
+import os.path
+
+env.hosts = ["100.27.3.38", "52.87.212.15"]
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
+
+
+def do_deploy(archive_path):
+    """Fabric script that distributes an archive to web servers"""
+
+    try:
+        if not os.path.isfile(archive_path):
+            return False
+        put(archive_path, "/tmp/")
+        file_name = os.path.basename(archive_path)
+        release = "/data/web_static/releases/{}".format(file_name[:-4])
+        run("mkdir -p {}".format(release))
+        run("tar -xz -f /tmp/{} -C {}".format(file_name, release))
+        run("rm /tmp/{}".format(file_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {} {}".format(release, "/data/web_static/current"))
+    except Exception:
+        return False
+
+    return True
