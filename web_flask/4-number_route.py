95887cdcea5cebec5b36423ef77bb16ec5008890
Initech <inimfonebong001@gmail.com>
2023-10-20 03:36:48 -0700

    correct pycodestyle

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,15 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """return Hello HBNB"""
     return ("Hello HBNB!")
 
+
 @app.route('/hbnb')
 def hbnb():
     """return HBNB"""
     return ("HBNB")
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
@@ -20,17 +23,20 @@ def c_route(text):
     text = text.replace("_", " ")
     return ("C {}".format(text))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     """response to python route"""
     return ("Python {}".format(text.replace("_", " ")))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """check if n is a number and return <n> is a number"""
     if isinstance(n, int):
         return ("{} is a number".format(n))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
