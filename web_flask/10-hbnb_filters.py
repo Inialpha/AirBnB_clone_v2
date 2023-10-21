f25dc748c579aab35f5ca60e34af3b787131f08b
Initech <inimfonebong001@gmail.com>
2023-10-21 00:32:57 -0700

    Addtask 10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module list all the states"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """list all the states or cities by states in the database"""
+
+    return (render_template('10-hbnb_filters.html'))
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """close current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
